d5d8277e9a9f0374a1f27cac6748543681824af8
yoonsuhcho <135435760+yoonsuhcho@users.noreply.github.com>
2025-10-12 23:12:17 +0900

    Create yoonsuh.c

---
+++ week06/3. b10816 - 숫자 카드 2/yoonsuh.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define OFFSET 10000000
+#define MAX 20000001  // -10,000,000 ~ 10,000,000 대응
+
+int countArr[MAX]; 
+int main() {
+    int N, M, num;
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &num);
+        countArr[num + OFFSET]++;  
+    }
+
+    scanf("%d", &M);
+    for (int i = 0; i < M; i++) {
+        scanf("%d", &num);
+        printf("%d ", countArr[num + OFFSET]);
+    }
+
+    return 0;
+}
