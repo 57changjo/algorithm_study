7c151796d05a315eca2ef001bb39a49aa246c40a
Minchang Cho <cmc507@naver.com>
2025-11-02 20:37:26 +0900

    p2 solved

--- week09/2. b10162 - 전자레인지/minchang.cpp
+++ week09/2. b10162 - 전자레인지/minchang.cpp
@@ -1,1 +1,40 @@
-// Do not delete this file
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+	
+using namespace std;
+
+int N;
+int A = 300, B = 60, C = 10;
+int aCnt, bCnt, cCnt;
+
+void inputData(void) {
+	cin >> N;
+}
+
+void solve(void) {
+	if (N % 10 != 0) {
+		cout << -1;
+		return;
+	}
+	aCnt = N / A;
+	N %= A;
+	bCnt = N / B;
+	N %= B;
+	cCnt = N / C;
+	N %= C;
+	cout << aCnt << " " << bCnt << " " << cCnt;
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	//(void)freopen("b10162.txt", "r", stdin);
+	inputData();
+	solve();
+
+	return 0;
+}
+
+
