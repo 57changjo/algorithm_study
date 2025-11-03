ae9dd18667baf765eefd3896fa0c2d28ce0785eb
0731chae <0731chae@gmail.com>
2025-11-03 12:38:26 +0900

    Update hyunjoon.cpp

--- week09/3. b14891 - 톱니바퀴/hyunjoon.cpp
+++ week09/3. b14891 - 톱니바퀴/hyunjoon.cpp
@@ -3,22 +3,22 @@
 #include <iostream>
 #include <string>
 
-#define MAX_N 100
-using namespace std; 
+using namespace std;
 
-int Clock[4+1][8+1];
+int Clock[4 + 1][8 + 1];
 int K;
 int Con[4];
 
 void spinclock(int dir, int* bearing) {
-	int start= 0, end = 0;
+	int start = 0, end = 0;
 	if (dir == 1) {								//시계
-		for (int i = 8; i > 0; i--) {
+		for (int i = 8; i >= 1; i--) {
 			if (i == 1) {
 				bearing[i] = end;
 			}
-			if (i == 8) {
-				int end = bearing[i];
+			else if (i == 8) {
+				end = bearing[i];
+				bearing[i] = bearing[i - 1];
 			}
 			else {
 				bearing[i] = bearing[i - 1];
@@ -28,7 +28,8 @@ void spinclock(int dir, int* bearing) {
 	if (dir == -1) {							//반시계
 		for (int i = 1; i <= 8; i++) {
 			if (i == 1) {
-				int start = bearing[i];
+				start = bearing[i];
+				bearing[i] = bearing[i + 1];
 			}
 			if (i == 8) {
 				bearing[i] = start;
@@ -40,19 +41,15 @@ void spinclock(int dir, int* bearing) {
 	}
 }
 
-int spinclock2(int dir, int* bearing) {
-	int start = 0, end = 0;
-	if (dir == 1) {
-		return bearing[8];
+void printclock(int* bearing) {				//디버깅용
+	for (int i = 1; i <= 8; i++) {
+		cout << bearing[i];
 	}
-	if (dir == -1) {							//반시계
-		return bearing[2];
-	}
-	else return bearing[1];
+	cout << '\n';
 }
 
 
-void scan_connect(int num) {
+void scan_connect() {					//톱니바퀴가 서로 돌릴 수 있는지(연결되어있는지) 확인
 	for (int i = 1; i <= 3; ++i) {
 		if (Clock[i][3] == Clock[i + 1][7]) {
 			Con[i] = 0;
@@ -61,14 +58,14 @@ void scan_connect(int num) {
 			Con[i] = 1;
 		}
 	}
+	//cout << Con[1] << Con[2] << Con[3]<< '\n';
 }
 
 void solve(int num, int dir) {
 	int dir_org = dir;
 	int pre_con = 1;
 	int R_start = num + 1, L_start = num - 1;
-	int cnt = 0;
-
+	scan_connect();
 	spinclock(dir, Clock[num]);
 
 	for (int i = R_start; i <= 4; ++i) {
@@ -76,10 +73,11 @@ void solve(int num, int dir) {
 		if (!Con[i - 1] || !pre_con) {
 			break;
 		}
+		//cout <<"stage debug_r : "<<i<<'\n';
 		spinclock(dir, Clock[i]);
-		pre_con = Con[i-1];
+		pre_con = Con[i - 1];
 	}
-	
+
 	dir = dir_org;
 	pre_con = 1;							//초기값 설정
 
@@ -88,6 +86,7 @@ void solve(int num, int dir) {
 		if (!Con[i] || !pre_con) {
 			break;
 		}
+		//cout << "stage debug_L : " << i << '\n';
 		spinclock(dir, Clock[i]);
 		pre_con = Con[i];
 	}
@@ -106,10 +105,10 @@ int value() {
 int main(void) {
 	int num = 0, dir = 0;
 	int cnt = 0;
-	(void)freopen("prob3.txt", "r", stdin);
+	//(void)freopen("prob3.txt", "r", stdin);
 	for (int i = 1; i <= 4; i++) {
 		for (int j = 1; j <= 8; j++) {
-			(void)scanf("%1d", Clock[i]+j);
+			(void)scanf("%1d", Clock[i] + j);
 		}
 	}
 	cin >> K;
@@ -118,10 +117,7 @@ int main(void) {
 		cin >> num >> dir;
 		solve(num, dir);
 	}
-
 	cout << value();
 
-	
 	return 0;
 }
-
