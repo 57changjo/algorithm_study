f1fd9850fbfdf75d650f1bf6e08f671a51297ee6
yoonsuhcho <135435760+yoonsuhcho@users.noreply.github.com>
2025-11-09 18:08:10 +0900

    Create yoonsuh.cpp

---
+++ week10/1. b2579 - 계단오르기/yoonsuh.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int stair[301];
+    int dp[301];
+
+    for (int i = 1; i <= n; i++)
+        cin >> stair[i];
+
+    dp[1] = stair[1];
+    dp[2] = stair[1] + stair[2];
+    for (int i = 3; i <= n; i++)
+        dp[i] = max(dp[i-2] + stair[i], dp[i-3] + stair[i-1] + stair[i]);
+
+    cout << dp[n];
+}
