40af1b6fabd2f1b5cc5b52fa2c303d4a5ef4cef6
pumpkin-9999 <108640107+pumpkin-9999@users.noreply.github.com>
2025-11-30 19:25:37 +0900

    Update sieun.cpp

--- week12/3. b14719 - 빗물/sieun.cpp
+++ week12/3. b14719 - 빗물/sieun.cpp
@@ -1,10 +1,4 @@
 #if 01
-/*
-* 블로그 풀이를 참고했습니다.
-현재의 height에서 양옆의 최대 높이를 구해서
-둘 중 작은 값에서 현재의 높이를 뺀 값을 계속해서 더하는 방식
-단, 첫번째 값과 마지막 값은 제외하도록 한다.
-*/
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -21,7 +15,8 @@ void inputData() {
 	}
 }
 
-int solve() {
+// 가장 직관적인 풀이 - O(N^2)
+int solve1() {
 	int rain = 0;
 
 	for (int i = 1; i < W - 1; i++) {
@@ -38,12 +33,44 @@ int solve() {
 	return rain;
 }
 
+// 투 포인터 풀어 - O(N)
+int solve2() {
+	int left = 0, right = W - 1;
+	int left_max = 0, right_max = 0;
+	int rain = 0;
+
+	while (left < right) {
+		// 왼쪽 - 더 낮은 쪽을 먼저
+		if (height[left] <= height[right]) {
+			if (height[left] >= left_max) {
+				left_max = height[left];
+			}
+			else {
+				rain += left_max - height[left];
+			}
+			left++;
+		}
+		// 오른쪽
+		else {
+			if (height[right] >= right_max) {
+				right_max = height[right];
+			}
+			else {
+				rain += right_max - height[right];
+			}
+			right--;
+		}
+	}
+
+	return rain;
+}
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
 	inputData();
-	cout << solve();
+	cout << solve2();
 
 	return 0;
 }
